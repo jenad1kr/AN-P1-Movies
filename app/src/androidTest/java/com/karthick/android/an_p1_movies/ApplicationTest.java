6723338779a53edbb0c3645c0f68412da95d68cd
jenad1kr <karthickraja@hotmail.com>
2015-11-01 09:27:51 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/karthick/android/an_p1_movies/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.karthick.android.an_p1_movies;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
